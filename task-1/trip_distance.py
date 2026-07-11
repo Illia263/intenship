daa842bb65bc8639cff9e0ea9b24fa759d39dbd8
Illia263 <matsykur.illya@gmail.com>
2026-07-11 14:16:20 +0200

    123

--- task-1/trip_distance.py
+++ task-1/trip_distance.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 file_path = 'task-1/yellow_tripdata_2026-03.parquet'
 
 df = pd.read_parquet(file_path)
@@ -12,5 +13,5 @@ payment_map = {
     6 : "Voided trip"
 }
 df['payment_type_name'] = df['payment_type'].map(payment_map)
-ans = df.groupby(['passenger_count', 'payment_type_name'])['trip_distance'].mean().round(2)
+ans = df.groupby(['passenger_count', 'payment_type_name']).agg({'trip_distance' : 'mean'}).round(2)
 print(ans)
