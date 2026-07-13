0a9a04721d7e79214e006c4b63cce910fe664c4b
Illia263 <matsykur.illya@gmail.com>
2026-07-13 22:58:28 +0200

    fix

--- task-2.5/my_gen.py
+++ task-2.5/my_gen.py
@@ -3,8 +3,10 @@ def gen():
     with open('task-2.5/dataset.csv', 'r') as f:
         yield from (float(row['total_amount']) *2 for row in csv.DictReader(f))
 my_gen = gen()
-print(next(my_gen))
-print(next(my_gen))
-print(next(my_gen))
-print(next(my_gen))
-print(next(my_gen))
+# print(next(my_gen))
+# print(next(my_gen))
+# print(next(my_gen))
+# print(next(my_gen))
+# print(next(my_gen))
+for f in range(5):
+    print(next(my_gen))
